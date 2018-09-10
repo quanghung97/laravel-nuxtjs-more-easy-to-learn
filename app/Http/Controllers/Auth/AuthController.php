46fa563463253a5da5521ab79514f482e5509e91
quanghung97 <hungcan1997@gmail.com>
2018-09-10 14:33:58 +0700

    remove JWT package and install laravel passport. should watch video https://www.youtube.com/playlist?list=PLzz9vf6075V06Vf-ytOUVOSyYQoi3jWOa.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,9 +3,68 @@
 namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
+use App\User;
 use App\Http\Controllers\Controller;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Hash;
 
-class \AuthController extends Controller
+class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'name' => 'required',
+            'password' => 'required'
+        ]);
+
+        $user = User::firstOrNew(['email'=>$request->email]);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        $guzzle = new Client;
+
+        $response = $guzzle->post(url('oauth/token'), [
+            'form_params' => [
+                'grant_type' => 'password',
+                'client_id' => '2',
+                'client_secret' => 'ElLyLtBPxqTRAb9nMWiUc0JOMvLT2oihFhpLoBKp',
+                'username' => $request->email,
+                'password' => $request->password,
+                'scope' => '',
+            ],
+        ]);
+
+        return response(['data' => json_decode((string) $response->getBody(), true)]);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+        if (!$user) {
+            return response(['status' => 'error', 'message' => 'User not found']);
+        } elseif (Hash::check($request->password, $user->password)) {
+            $guzzle = new Client;
+
+            $response = $guzzle->post(url('oauth/token'), [
+                'form_params' => [
+                    'grant_type' => 'password',
+                    'client_id' => '2',
+                    'client_secret' => 'ElLyLtBPxqTRAb9nMWiUc0JOMvLT2oihFhpLoBKp',
+                    'username' => $request->email,
+                    'password' => $request->password,
+                    'scope' => '',
+                ],
+            ]);
+
+            return response(['data' => json_decode((string) $response->getBody(), true)]);
+        }
+    }
 }
--- app/User.php
+++ app/User.php
@@ -2,14 +2,14 @@
 
 namespace App;
 
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
-use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -28,14 +28,4 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
 }
